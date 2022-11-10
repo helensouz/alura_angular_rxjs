a8e0c7d1f97426e9af037fac1773e8924161f462
Helen Oliveira <sousahelen816@gmail.com>
2022-11-10 13:40:20 -0300

    get

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
+import {Acoes} from './modelo/acoes';
+import {AcoesService} from './acoes.service';
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent implements OnInit {
+  acoes: Acoes
   acoesInput = new FormControl();
 
-  constructor() {}
+  constructor(private acoesService: AcoesService) {}
+
+  ngOnInit() {
+    this.acoesService.getAcoes().subscribe((data) => {
+      this.acoes = data.payload
+    }) 
+  }
 }
